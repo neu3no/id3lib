8a1ded305d557fc80affc948ab9e7c089a16489c
neu3no <the@neu3no.de>
2014-09-14 10:12:24 +0200

    minor fixes

--- id3_frame_parser.js
+++ id3_frame_parser.js
@@ -1,97 +1,99 @@
 /*
-* Author
-*         Christian Neubauer
-* web: http://neu3no.de/         twitter: @neu3no mail: the@neu3no.de
-*         
-* Licence
-* (cc-by-nc) 2012
-*         This work is licensed under the Creative Commons Attribution-NonCommercial
-* 3.0 Unported License. To view a copy of this license, visit
-* http://creativecommons.org/licenses/by-nc/3.0/.
-*
-* Project
-*         website: http://neu3no.de/
-*         github : https://github.com/neu3no/id3lib
-*
-*/
-var conf=require("./id3conf.json");
-var helper=require("./id3_helper.js");
-var zlib=require("zlib");
+ * Author
+ *         Christian Neubauer
+ * web: http://neu3no.de/         twitter: @neu3no mail: the@neu3no.de
+ *
+ * Licence
+ * (cc-by-nc) 2012
+ *         This work is licensed under the Creative Commons Attribution-NonCommercial
+ * 3.0 Unported License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc/3.0/.
+ *
+ * Project
+ *         website: http://neu3no.de/
+ *         github : https://github.com/neu3no/id3lib
+ *
+ */
+var conf = require("./id3conf.json");
+var helper = require("./id3_helper.js");
+var zlib = require("zlib");
 
 // pre processing
-exports.preProcess =function(frame,ver,callback){
-  var buffer = new Buffer(frame.payload);
-  var vconf=conf.versions["2"][ver];
-  var flags={};
-  var usize = 0;
-  
-  if (ver > 2){
-    helper.arrtag(buffer[0],vconf.framehead.flag0,flags);
-    helper.arrtag(buffer[1],vconf.framehead.flag1,flags);
-    buffer=buffer.slice(2);
-        
-    if (flags.compression || flags.datalengthindicators) {
-      var usize = buffer.readInt32BE(0);
-      buffer=buffer.slice(4);
-    }
-    
-    if (flags.compression){
-      zlib.gunzip(buffer, function(a,buf){
-        callback({},{
-          "id" : frame.id,
-          "version" : ver,
-          "flags" : flags, 
-          "decomp" : "true",
-          "payload": buffer
-        });
-      });
-      return;
-    }
-  }
-  callback({},{
-    "id" : frame.id,
-    "version" : ver,
-    "flags" : flags, 
-    "decomp" : "false",
-    "payload": buffer
-  });
-}
+exports.preProcess = function (frame, ver, callback) {
+	var buffer = new Buffer(frame.payload);
+	var vconf = conf.versions["2"][ver];
+	var flags = {};
+	//var usize = 0;
 
-exports.parser ={};
+	if (ver > 2) {
+		helper.arrtag(buffer[0], vconf.framehead.flag0, flags);
+		helper.arrtag(buffer[1], vconf.framehead.flag1, flags);
+		buffer = buffer.slice(2);
 
-exports.parser["text"] ={
-  "condition" : function(frame){
-    var tid = frame.id;
-    return tid[0] =="T" && tid != "TXX" && tid != "TXXX";
-  },
-  
-  "parse" : function(frame,ver){
-    var buffer = new Buffer(frame.payload);
-    var encdic = conf.versions["2"][ver].encodings;  
-    var flags=frame.flags;
-        
-    if (typeof(encdic[buffer[0]])!=="undefined") {
-      if (encdic[buffer[0]] === "ucs2"){
-        buffer=buffer.slice(2);
-      }
-      var ret= {
-        "id" : frame.id,
-        "flags" : flags,
-        "payload" : buffer.slice(1).toString(encdic[buffer[0]])
-      };
-      return ret;    
-    } else {
-      return {};
-    }
-  }
+		if (flags.compression || flags.datalengthindicators) {
+			//usize = buffer.readInt32BE(0);
+			buffer = buffer.slice(4);
+		}
+
+		if (flags.compression) {
+			zlib.gunzip(buffer, function (){ //a, buf) {
+				callback({}, {
+					"id": frame.id,
+					"version": ver,
+					"flags": flags,
+					"decomp": "true",
+					"payload": buffer
+				});
+			});
+			return;
+		}
+	}
+	callback({}, {
+		"id": frame.id,
+		"version": ver,
+		"flags": flags,
+		"decomp": "false",
+		"payload": buffer
+	});
 };
 
-exports.parser["default"]={
-  "condition" : function(){return true;},
-  "parse" : function(frame,ver){
-    return {
-      "id" : frame.id,
-      "payload" : frame.payload.toString("hex")
-    }
-  }
-}
+exports.parser = {};
+
+exports.parser["text"] = {
+	"condition": function (frame) {
+		var tid = frame.id;
+		return tid[0] == "T" && tid != "TXX" && tid != "TXXX";
+	},
+
+	"parse": function (frame, ver) {
+		var buffer = new Buffer(frame.payload);
+		var encdic = conf.versions["2"][ver].encodings;
+		var flags = frame.flags;
+
+		if (typeof(encdic[buffer[0]]) !== "undefined") {
+			if (encdic[buffer[0]] === "ucs2") {
+				buffer = buffer.slice(2);
+			}
+			return {
+				"id": frame.id,
+				"flags": flags,
+				"payload": buffer.slice(1).toString(encdic[buffer[0]])
+			};
+		} else {
+			return {};
+		}
+	}
+};
+
+exports.parser["default"] = {
+	"condition": function () {
+		return true;
+	},
+	"parse": function (frame, ver) {
+		return {
+			"id": frame.id,
+			"payload": frame.payload.toString("hex"),
+			"version" : ver
+		}
+	}
+};
--- id3v2.js
+++ id3v2.js
@@ -1,216 +1,213 @@
 /*
-* Author
-*         Christian Neubauer
-* web: http://neu3no.de/         twitter: @neu3no mail: the@neu3no.de
-*         
-* Licence
-* (cc-by-nc) 2012
-*         This work is licensed under the Creative Commons Attribution-NonCommercial
-* 3.0 Unported License. To view a copy of this license, visit
-* http://creativecommons.org/licenses/by-nc/3.0/.
-*
-* Project
-*         website: http://neu3no.de/
-*         github : https://github.com/neu3no/id3lib
-*
-*/
+ * Author
+ *         Christian Neubauer
+ * web: http://neu3no.de/         twitter: @neu3no mail: the@neu3no.de
+ *
+ * Licence
+ * (cc-by-nc) 2012
+ *         This work is licensed under the Creative Commons Attribution-NonCommercial
+ * 3.0 Unported License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc/3.0/.
+ *
+ * Project
+ *         website: http://neu3no.de/
+ *         github : https://github.com/neu3no/id3lib
+ *
+ */
 var fs = require('fs');
 
 var Buffer = require('buffer').Buffer;
-var conf=require("./id3conf.json");
-var helper=require("./id3_helper.js");
-var parser=require("./id3_frame_parser.js");
+var conf = require("./id3conf.json");
+var helper = require("./id3_helper.js");
+var parser = require("./id3_frame_parser.js");
 
 //
 
-var grabExtHead = function(f, head, callback){
-  var buffer = new Buffer(conf.versions["2"][head.ver].extheader.maxsize);
-  // in the beginning read a buffer with maximum count of byte a extheader could 
-  // have, we can strip it down later
-  fs.read(f, buffer,0,conf.versions["2"][head.ver].extheader.maxsize,conf.versions["2"].headsize,function(rerr, num){
-    var size = conf.versions["2"][head.ver].extheader.maxsize;
-    if (conf.versions["2"][head.ver].extheader.calcsize) {
-      size = 
-        conf.versions["2"][head.ver].extheader.sizebase + 
-        helper.sizecalc(buffer.slice(0,4));
-    } else {
-      size = 
-        conf.versions["2"][head.ver].extheader.sizebase + 
-        buffer.readInt32BE(0);
-    }
-    if (size > conf.versions["2"][head.ver].extheader.maxsize) {
-      callback({"code" : 5, "dscr" : "extended header size is too big!"},undefined);
-      return;
-    }
-    // since we have the size we can strip the buffer to header size
-    buffer=buffer.slice(0,size);
-    
-    callback({},buffer);
-  });
+var grabExtHead = function (f, head, callback) {
+	var buffer = new Buffer(conf.versions["2"][head.ver].extheader.maxsize);
+	// in the beginning read a buffer with maximum count of byte a extheader could
+	// have, we can strip it down later
+	fs.read(f, buffer, 0, conf.versions["2"][head.ver].extheader.maxsize, conf.versions["2"].headsize, function (){ //rerr, num) {
+		var size = conf.versions["2"][head.ver].extheader.maxsize;
+		if (conf.versions["2"][head.ver].extheader.calcsize) {
+			size =
+				conf.versions["2"][head.ver].extheader.sizebase +
+				helper.sizecalc(buffer.slice(0, 4));
+		} else {
+			size =
+				conf.versions["2"][head.ver].extheader.sizebase +
+				buffer.readInt32BE(0);
+		}
+		if (size > conf.versions["2"][head.ver].extheader.maxsize) {
+			callback({"code": 5, "dscr": "extended header size is too big!"}, undefined);
+			return;
+		}
+		// since we have the size we can strip the buffer to header size
+		buffer = buffer.slice(0, size);
+
+		callback({}, buffer);
+	});
 };
 
-var grabHead = function(f, callback){
-  var buffer = new Buffer(conf.versions["2"].headsize);
-  fs.read(f, buffer,0,conf.versions["2"].headsize,0,function(rerr, num){
-    // test wether "ID3" string is present or not.if not there might be no tag
-    if (buffer.toString('utf8',0,3)!="ID3"){
-			callback({"code" : 2, "dscr":"no ID3v2 tag found"},undefined);
-			return;
+var grabHead = function (f, callback) {
+	var buffer = new Buffer(conf.versions["2"].headsize);
+	fs.read(f, buffer, 0, conf.versions["2"].headsize, 0, function (){ //rerr, num) {
+		// test wether "ID3" string is present or not.if not there might be no tag
+		if (buffer.toString('utf8', 0, 3) != "ID3") {
+			callback({"code": 2, "dscr": "no ID3v2 tag found"}, undefined);
 		} else {
-		  callback({},buffer);
+			callback({}, buffer);
 		}
-  });
+	});
 };
 
 // grab the head and make a js object out of it for versions described in conf
-var getID3v2head = function(f, callback){
-   grabHead(f,function(err,buffer){
-    if (err.code) {
-      callback(err, undefined);  
-      return;
-    } else {
-      var head={};
-      
-      head.ver  = buffer[3];
-      head.rev  = buffer[4];
-      
-      // check if we have a description of this version in config
-      if (conf.versions["2"]["versions"].indexOf(head.ver) == -1) {
-        callback(
-          {"code":3,"dscr":"ID3v2." + head.ver + " not implemented"},
-          undefined
-        );
-        return;
-        
-      } else {
-      
-        head.flags={};
-        // map flags according to the definition in config file
-        helper.arrtag(
-          buffer[5],
-          conf.versions["2"][head.ver].header.flags, // see config for that
-          head.flags
-        );
-        
-        // if the size is stored as 4*7Bit it needs to be converted
-        if (conf.versions["2"][head.ver].header.calcsize){
-          head.size = helper.sizecalc(buffer.slice(6,10));
-        } else {
-          // if not we can just read for bytes and interpret as 32bit int
-          head.size = buffer.readInt32BE(6);
-        }
-        
-        callback({},head);
-      }
-    }
-  });
+var getID3v2head = function (f, callback) {
+	grabHead(f, function (err, buffer) {
+		if (err.code) {
+			callback(err, undefined);
+		} else {
+			var head = {};
+
+			head.ver = buffer[3];
+			head.rev = buffer[4];
+
+			// check if we have a description of this version in config
+			if (conf.versions["2"]["versions"].indexOf(head.ver) == -1) {
+				callback(
+					{"code": 3, "dscr": "ID3v2." + head.ver + " not implemented"},
+					undefined
+				);
+			} else {
+
+				head.flags = {};
+				// map flags according to the definition in config file
+				helper.arrtag(
+					buffer[5],
+					conf.versions["2"][head.ver].header.flags, // see config for that
+					head.flags
+				);
+
+				// if the size is stored as 4*7Bit it needs to be converted
+				if (conf.versions["2"][head.ver].header.calcsize) {
+					head.size = helper.sizecalc(buffer.slice(6, 10));
+				} else {
+					// if not we can just read for bytes and interpret as 32bit int
+					head.size = buffer.readInt32BE(6);
+				}
+
+				callback({}, head);
+			}
+		}
+	});
 };
 
-var grabFrameRecur = function(buffer,head){
-  var len =conf.versions["2"][head.ver].framehead.wordsize; // keep readability
-  var sizb=conf.versions["2"][head.ver].framehead.sizebase; // keep readability
-  var size;  // int
-  var frame; // buffer
-  
-  size= buffer.slice(len,2*len).readBigInt() + sizb;
-  if (size > 0 && buffer.slice(0,1).toString("hex") != "00") { // just if we trapped into undeclared padding
-    frame = new Buffer(buffer.slice(0,size));
-    // there is still data in the buffer -> recall our self and append the result
+var grabFrameRecur = function (buffer, head) {
+	var len = conf.versions["2"][head.ver].framehead.wordsize; // keep readability
+	var sizb = conf.versions["2"][head.ver].framehead.sizebase; // keep readability
+	var size;  // int
+	var frame; // buffer
 
-    if (buffer.slice(size).length>2*len) {
-      return [frame].concat(grabFrameRecur(buffer.slice(size),head));
-    
-    // theres no data left in buffer -> finish -> no more function call
-    } else {
-      return [frame];
-    }
-    
-  // if the size is 0 we trapped into padding and could stop recursing
-  } else {
-    return [];
-  }
+	size = buffer.slice(len, 2 * len).readBigInt() + sizb;
+	if (size > 0 && buffer.slice(0, 1).toString("hex") != "00") { // just if we trapped into undeclared padding
+		frame = new Buffer(buffer.slice(0, size));
+		// there is still data in the buffer -> recall our self and append the result
+
+		if (buffer.slice(size).length > 2 * len) {
+			return [frame].concat(grabFrameRecur(buffer.slice(size), head));
+
+			// theres no data left in buffer -> finish -> no more function call
+		} else {
+			return [frame];
+		}
+
+		// if the size is 0 we trapped into padding and could stop recursing
+	} else {
+		return [];
+	}
 };
 
-var getFrames = function(f, head,offset,callback){
-  var tmp=[];
-  grabFrames(f,head,offset, function(e,frames){
-    for (framekey in frames){       // sure not the fastest solution
-      var frame = frames[framekey]; // but most flexible and most readable
-      
-      for ( p in parser.parser) {
-      
-        if (parser.parser[p].condition(frame)){
-          tmp.push(parser.parser[p].parse(frame,head.ver));
-          break; 
-        }
-        
-      }
-    }
-    
-    callback({},{"header": head, "frames" : tmp});
-  });
+var getFrames = function (f, head, offset, callback) {
+	var tmp = [];
+	grabFrames(f, head, offset, function (e, frames) {
+		for (var framekey in frames) {       // sure not the fastest solution
+			if (! frames.hasOwnProperty(framekey)) continue;
+			var frame = frames[framekey]; // but most flexible and most readable
+
+			for (p in parser.parser) {
+				if (!parser.parser.hasOwnProperty(p)) continue;
+				if (parser.parser[p].condition(frame)) {
+					tmp.push(parser.parser[p].parse(frame, head.ver));
+					break;
+				}
+
+			}
+		}
+
+		callback({}, {"header": head, "frames": tmp});
+	});
 };
 
-var grabFrames = function(f,head,offset,callback){
-  var buffer = new Buffer(head.size - offset);
-  fs.read(f,buffer,0,buffer.length,offset,function(rerr,num){
-    var frames = grabFrameRecur(buffer,head);
-    var len =conf.versions["2"][head.ver].framehead.wordsize; // keep readability
-    var tmp=[];
-    
-    for (i=0; i<frames.length; i++){
-      var frame=frames[i];
-      parser.preProcess({
-          "id"     : frame.slice(0,len).toString(),
-          "payload": frame.slice(2*len)
-        }, head.ver, function(err,preframe){
-          tmp.push(preframe);
-      });
-    }
-    callback({},tmp);
-  });
+var grabFrames = function (f, head, offset, callback) {
+	var buffer = new Buffer(head.size - offset);
+	fs.read(f, buffer, 0, buffer.length, offset, function (){ //rerr, num) {
+		var frames = grabFrameRecur(buffer, head);
+		var len = conf.versions["2"][head.ver].framehead.wordsize; // keep readability
+		var tmp = [];
+
+		for (i = 0; i < frames.length; i++) {
+			var frame = frames[i];
+			parser.preProcess({
+				"id": frame.slice(0, len).toString(),
+				"payload": frame.slice(2 * len)
+			}, head.ver, function (err, preframe) {
+				tmp.push(preframe);
+			});
+		}
+		callback({}, tmp);
+	});
 };
 
-exports.read = function(filename, callback){
-  var closeback = function(fd,e,d){
-    fs.close(fd,function(){
-      callback(e,d);
-    });
-  };
-  if (typeof filename !== "string"){
-    callback({"dscr": "typeof filename =" + (typeof filename)},{})
-    return;
-  }
-  fs.exists(filename,function(ex){
-    fs.open(filename,'r',function(err,f){
-    
-      if (err) {
-        closeback(f,{"code" :1, "dscr" : "error opening file", "err": err},undefined);
-        return;
-      }
-      
-      getID3v2head(f,function(e,head){
-        // we need the offset where the frames start
-        var offset=conf.versions["2"].headsize;
-        if (typeof head === "undefined" ) {
-          closeback(f,{"code":99,"filename":filename},{});
-          return;
-        }
-        if (head.flags.extendedheader){
-          // TODO: parse extended header
-          grabExtHead(f,head,function(ee,eh){
-            offset+=eh.length;
-            getFrames(f,head,offset,function(e,d){
-              closeback(f,e,d);
-            });
-          });
-        } else{
-            getFrames(f,head,offset,function(e,d){
-              closeback(f,e,d);
-            });
-        }
-      });
-    });
-  });
-  
+exports.read = function (filename, callback) {
+	var closeback = function (fd, e, d) {
+		fs.close(fd, function () {
+			callback(e, d);
+		});
+	};
+	if (typeof filename !== "string") {
+		callback({"dscr": "typeof filename =" + (typeof filename)}, {});
+		return;
+	}
+	fs.exists(filename, function (){ //ex) {
+		fs.open(filename, 'r', function (err, f) {
+
+			if (err) {
+				closeback(f, {"code": 1, "dscr": "error opening file", "err": err}, undefined);
+				return;
+			}
+
+			getID3v2head(f, function (e, head) {
+				// we need the offset where the frames start
+				var offset = conf.versions["2"].headsize;
+				if (typeof head === "undefined") {
+					closeback(f, {"code": 99, "filename": filename}, {});
+					return;
+				}
+				if (head.flags.extendedheader) {
+					// TODO: parse extended header
+					grabExtHead(f, head, function (ee, eh) {
+						offset += eh.length;
+						getFrames(f, head, offset, function (e, d) {
+							closeback(f, e, d);
+						});
+					});
+				} else {
+					getFrames(f, head, offset, function (e, d) {
+						closeback(f, e, d);
+					});
+				}
+			});
+		});
+	});
+
 };
